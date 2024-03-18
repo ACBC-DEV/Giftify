e7b2cf1f1909c1d2d99a6e1e0a31b71e03758442
ACBC <acbc.dev@gmail.com>
2024-03-18 17:49:13 -0500

    Agregado componente FormSearch para búsqueda con autocompletado

---
+++ src/components/FormSearch.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useDebounce } from "use-debounce";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+
+export default function FormSearch({ search }: { search: string }) {
+  const router = useRouter();
+  const [text, setText] = useState(search);
+  const [query] = useDebounce(text, 1000);
+
+  useEffect(() => {
+    if (query) {
+      router.push(`/?q=${query}`);
+    }
+  }, [query, router]);
+
+  return (
+    <div className="grid place-content-center my-12">
+      <Input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          e.preventDefault();
+          setText(e.target.value);
+          //setSearch(search);
+        }}
+        className="bg-transparent border-4 border-white rounded-3xl px-4 py-6 h-12 w-80 text-lg  hover:bg-white hover:text-black  duration-300 ease-in-out"
+      />
+    </div>
+  );
+}
