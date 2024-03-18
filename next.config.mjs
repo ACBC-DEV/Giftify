81d201cdfd3aa94d840139ea2d0d02bc78a27f46
ACBC <acbc.dev@gmail.com>
2024-03-18 17:48:20 -0500

    Agrega patrones remotos para imágenes en next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media1.giphy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "giphy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media2.giphy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media3.giphy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media4.giphy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media5.giphy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media6.giphy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media0.giphy.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
