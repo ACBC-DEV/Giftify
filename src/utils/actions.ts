e379725df346e8b1692fa33987e08f8fca460150
ACBC <acbc.dev@gmail.com>
2024-03-18 17:50:32 -0500

    Agregado archivo actions.ts con función getGifs() para obtener gifs mediante una solicitud al servidor

---
+++ src/utils/actions.ts
@@ -0,0 +1,12 @@
+"use server";
+import { ResponseData } from "@/types";
+export async function getGifs(q: string, { offset = 0, limit = 25 } = {}) {
+  //console.log(offset, limit);
+
+  const rta = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}?q=${q}&offset=${offset}&limit=${limit}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+  );
+  const data: ResponseData = await rta.json();
+
+  return data;
+}
