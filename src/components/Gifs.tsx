e79f4befbd5a1542a9278038595e00915b681bc3
acbc <acbc.dev@gmail.com>
2024-06-02 20:11:56 -0500

    Refactor Gifs component to use type import for GifData

--- src/components/Gifs.tsx
+++ src/components/Gifs.tsx
@@ -1,16 +1,16 @@
 import { getGifs } from "@/actions/actions";
-import { GifData } from "@/types";
+import type { GifData } from "@/types";
 import InfinityScroll from "./InfinityScroll";
 
 interface GifState {
-  data: GifData[];
-  offset: number;
+	data: GifData[];
+	offset: number;
 }
 export default async function Gifs({ q }: { q: string }) {
-  const { data } = await getGifs(q);
-  return (
-    <section key={Math.random()} className="rounded-lg">
-      <InfinityScroll initialGifs={q ? data : []} search={q} />
-    </section>
-  );
+	const { data } = await getGifs(q);
+	return (
+		<section key={Math.random()} className="rounded-lg">
+			<InfinityScroll initialGifs={q ? data : []} search={q} />
+		</section>
+	);
 }
