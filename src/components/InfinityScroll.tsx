a504a099b6f04f0ce4f455c9937b89a1f7c732be
acbc <acbc.dev@gmail.com>
2024-06-02 20:12:01 -0500

    Refactor InfinityScroll component to use type import for GifData and improve code readability

--- src/components/InfinityScroll.tsx
+++ src/components/InfinityScroll.tsx
@@ -1,52 +1,53 @@
 "use client";
 
-import { GifData } from "@/types";
+import type { GifData } from "@/types";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { getGifs } from "@/actions/actions";
 import LoadingImage from "./LoadingImage";
 
 export default function InfinityScroll({
-  initialGifs,
-  search,
+	initialGifs,
+	search,
 }: {
-  initialGifs: GifData[];
-  search: string;
+	initialGifs: GifData[];
+	search: string;
 }) {
-  const [gifs, setGifs] = useState(initialGifs);
-  const [offset, setOffset] = useState(0);
+	const [gifs, setGifs] = useState(initialGifs);
+	const [offset, setOffset] = useState(0);
 
-  const { ref, inView } = useInView();
+	const { ref, inView } = useInView();
 
-  const getMoreGifs = async () => {
-    const next = offset + 25;
-    const { data } = await getGifs(search, { offset: next });
+	const getMoreGifs = async () => {
+		const next = offset + 25;
+		const { data } = await getGifs(search, { offset: next });
 
-    if (data.length === 0) return;
-    setGifs((prev) => [...prev, ...data]);
-    setOffset(next);
-  };
-  useEffect(() => {
-    if (inView) {
-      getMoreGifs();
-    }
-  }, [inView]);
+		if (data.length === 0) return;
+		setGifs((prev) => [...prev, ...data]);
+		setOffset(next);
+	};
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		if (inView) {
+			getMoreGifs();
+		}
+	}, [inView]);
 
-  return (
-    <>
-      <ul className="flex flex-wrap justify-center items-center ">
-        {gifs.map((gif) => (
-          <LoadingImage
-            key={gif.id}
-            src={gif.images.original.webp || gif.images.original.url}
-            alt={gif.title}
-            width={250}
-            height={250}
-          />
-        ))}
-      </ul>
+	return (
+		<>
+			<ul className="flex flex-wrap justify-center items-center ">
+				{gifs.map((gif) => (
+					<LoadingImage
+						key={gif.id}
+						src={gif.images.original.webp || gif.images.original.url}
+						alt={gif.title}
+						width={250}
+						height={250}
+					/>
+				))}
+			</ul>
 
-      {!!search && <span ref={ref} className="loader my-10"></span>}
-    </>
-  );
+			{!!search && <span ref={ref} className="loader my-10" />}
+		</>
+	);
 }
