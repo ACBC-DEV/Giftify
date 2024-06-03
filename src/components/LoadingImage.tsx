57a62a4126970b6c9d1f82332ed23504729ad6c1
acbc <acbc.dev@gmail.com>
2024-06-02 20:12:07 -0500

    chore: Refactor LoadingImage component for improved code readability and performance

--- src/components/LoadingImage.tsx
+++ src/components/LoadingImage.tsx
@@ -3,51 +3,51 @@ import { useState } from "react";
 import Image from "next/image";
 
 interface LoadingImageProps {
-  src: string;
-  alt: string;
-  width: number;
-  height: number;
+	src: string;
+	alt: string;
+	width: number;
+	height: number;
 }
 
 const LoadingImage: React.FC<LoadingImageProps> = ({
-  src,
-  alt,
-  width,
-  height,
+	src,
+	alt,
+	width,
+	height,
 }) => {
-  const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(true);
 
-  const randomColorTl = () => {
-    const colorS = [
-      "bg-red-500",
-      "bg-yellow-500",
-      "bg-green-500",
-      "bg-blue-500",
-      "bg-indigo-500",
-    ];
-    const randomColor = colorS[Math.floor(Math.random() * colorS.length)];
-    return randomColor;
-  };
-  return (
-    <div style={{ position: "relative", width, height }}>
-      {isLoading && (
-        <div
-          style={{ width: `${width}px`, height: `${height}px` }}
-          className={`animate-pulse ${randomColorTl()}`}
-        />
-      )}
-      <picture>
-        <Image
-          src={src}
-          alt={alt}
-          fill={true}
-          className={`size-full object-cover  `}
-          unoptimized
-          onLoad={() => setIsLoading(false)}
-        />
-      </picture>
-    </div>
-  );
+	const randomColorTl = () => {
+		const colorS = [
+			"bg-red-500",
+			"bg-yellow-500",
+			"bg-green-500",
+			"bg-blue-500",
+			"bg-indigo-500",
+		];
+		const randomColor = colorS[Math.floor(Math.random() * colorS.length)];
+		return randomColor;
+	};
+	return (
+		<div style={{ position: "relative", width, height }}>
+			{isLoading && (
+				<div
+					style={{ width: `${width}px`, height: `${height}px` }}
+					className={`animate-pulse ${randomColorTl()}`}
+				/>
+			)}
+			<picture>
+				<Image
+					src={src}
+					alt={alt}
+					fill={true}
+					className="size-full object-cover"
+					unoptimized
+					onLoad={() => setIsLoading(false)}
+				/>
+			</picture>
+		</div>
+	);
 };
 
 export default LoadingImage;
