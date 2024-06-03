e396fa4bf443a49676751102784d54da65b7df40
acbc <acbc.dev@gmail.com>
2024-06-02 20:11:50 -0500

    Refactor FormSearch component to use debounced search query and improve code readability

--- src/components/FormSearch.tsx
+++ src/components/FormSearch.tsx
@@ -5,28 +5,27 @@ import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 
 export default function FormSearch({ search }: { search: string }) {
-  const router = useRouter();
-  const [text, setText] = useState(search);
-  const [query] = useDebounce(text, 1000);
+	const router = useRouter();
+	const [text, setText] = useState(search);
+	const [query] = useDebounce(text, 1000);
 
-  useEffect(() => {
-    if (query) {
-      router.push(`/?q=${query}`);
-    }
-  }, [query, router]);
+	useEffect(() => {
+		if (query) {
+			router.push(`/?q=${query}`);
+		}
+	}, [query, router]);
 
-  return (
-    <div className="grid place-content-center my-12">
-      <Input
-        type="text"
-        value={text}
-        onChange={(e) => {
-          e.preventDefault();
-          setText(e.target.value);
-          //setSearch(search);
-        }}
-        className="bg-transparent border-4 border-white rounded-3xl focus-visible:ring-0 focus:outline-none focus:ring-offset-transparent px-4 py-6 h-12 w-80 text-lg  hover:bg-white hover:text-black  duration-300 ease-in-out"
-      />
-    </div>
-  );
+	return (
+		<div className="grid place-content-center my-12">
+			<Input
+				type="text"
+				value={text}
+				onChange={(e) => {
+					e.preventDefault();
+					setText(e.target.value);
+				}}
+				className="bg-transparent border-4 border-white rounded-3xl focus-visible:ring-0 focus:outline-none focus:ring-offset-transparent px-4 py-6 h-12 w-80 text-lg  hover:bg-white hover:text-black  duration-300 ease-in-out"
+			/>
+		</div>
+	);
 }
